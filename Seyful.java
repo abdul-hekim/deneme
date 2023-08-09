71332c4562123fc84aad3eb892f39995124d84de
abdul-hekim <141836282+abdul-hekim@users.noreply.github.com>
2023-08-09 18:40:02 +0300

    first commit

---
+++ Seyful.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.seyful;
+
+/**
+ *
+ * @author khabi
+ */
+public class Seyful {
+
+    public static void main(String[] args) {
+        System.out.println("seyfullah abi naber");
+    }
+}
